f6413eb73bc20b496a54a45d7abc6787d1228e6e
Piksqu <65417571+breadbug23@users.noreply.github.com>
2021-09-12 11:17:56 +0200

    Update taser.js

--- scripts/units/taser.js
+++ scripts/units/taser.js
@@ -2,7 +2,7 @@ const taser = extend(UnitType, "taser", {
 	draw(unit){
 		this.super$draw(unit);
 		Draw.rect("somas-taser-orb", unit.x, unit.y, Time.time * 12);
-		Draw.rect("somas-taser-orb-outline", unit.x, unit.y, Time.time * 12);
+		Draw.rect("somas-taser-orb-2", unit.x, unit.y, Time.time * -14);
 	}
 });
 taser.constructor = () => extend(UnitEntity, {});
