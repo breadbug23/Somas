06942793905679e62111c6f0b38d19d558e730c0
Piksqu <65417571+breadbug23@users.noreply.github.com>
2021-09-12 11:21:53 +0200

    Update storm.js

--- scripts/units/storm.js
+++ scripts/units/storm.js
@@ -2,15 +2,10 @@ const storm = extend(UnitType, "storm", {
 	draw(unit){
 		this.super$draw(unit);
 		Draw.rect("somas-storm-orb", unit.x, unit.y, Time.time * 12);
-		Draw.rect("somas-storm-orb", unit.x, unit.y, (Time.time + 6) * 12);
-		Draw.rect("somas-storm-orb", unit.x, unit.y, (Time.time - 6) * 12);
-		Draw.rect("somas-storm-orb", unit.x, unit.y, (Time.time + 12) * 12);
-		Draw.rect("somas-storm-orb", unit.x, unit.y, (Time.time - 12) * 12);
-		Draw.rect("somas-storm-orb-outline", unit.x, unit.y, Time.time * 12);
-		Draw.rect("somas-storm-orb-outline", unit.x, unit.y, (Time.time + 6) * 12);
-		Draw.rect("somas-storm-orb-outline", unit.x, unit.y, (Time.time - 6) * 12);
-		Draw.rect("somas-storm-orb-outline", unit.x, unit.y, (Time.time + 12) * 12);
-		Draw.rect("somas-storm-orb-outline", unit.x, unit.y, (Time.time - 12) * 12);
+		Draw.rect("somas-storm-orb", unit.x, unit.y, (Time.time + 10) * 12);
+		Draw.rect("somas-storm-orb", unit.x, unit.y, (Time.time - 10) * 12);
+		Draw.rect("somas-storm-orb-2", unit.x, unit.y, Time.time * -14);
+		Draw.rect("somas-storm-orb-2", unit.x, unit.y, (Time.time + 6.5) * -14);
 	}
 });
 storm.constructor = () => extend(UnitEntity, {});
