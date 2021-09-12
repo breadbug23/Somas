36a3080c1c43486028bf0d1604ea7afd4123aaae
Piksqu <65417571+breadbug23@users.noreply.github.com>
2021-09-12 11:21:20 +0200

    Update lightning.js

--- scripts/units/lightning.js
+++ scripts/units/lightning.js
@@ -2,11 +2,8 @@ const lightning = extend(UnitType, "lightning", {
 	draw(unit){
 		this.super$draw(unit);
 		Draw.rect("somas-lightning-orb", unit.x, unit.y, Time.time * 12);
-		Draw.rect("somas-lightning-orb", unit.x, unit.y, (Time.time + 10) * 12);
-		Draw.rect("somas-lightning-orb", unit.x, unit.y, (Time.time - 10) * 12);
-		Draw.rect("somas-lightning-orb-outline", unit.x, unit.y, Time.time * 12);
-		Draw.rect("somas-lightning-orb-outline", unit.x, unit.y, (Time.time + 10) * 12);
-		Draw.rect("somas-lightning-orb-outline", unit.x, unit.y, (Time.time - 10) * 12);
+		Draw.rect("somas-lightning-orb-2", unit.x, unit.y, (Time.time + 10) * -14);
+		Draw.rect("somas-lightning-orb", unit.x, unit.y, (Time.time - 8) * 12);
 	}
 });
 lightning.constructor = () => extend(UnitEntity, {});
