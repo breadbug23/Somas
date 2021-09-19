d1516c53b53001b800057a1e444f9131595992c2
Piksqu <65417571+breadbug23@users.noreply.github.com>
2021-09-19 14:31:42 +0200

    Delete plastaniumroombamecha.js

--- scripts/units/plastaniumroombamecha.js
+++
@@ -1,3 +0,0 @@
-const plastaniumroombamecha = extend(UnitType, "plastaniumroombamecha", {});
-plastaniumroombamecha.defaultController = () => new MinerAI()
-plastaniumroombamecha.constructor = () => extend(LegsUnit, {});
