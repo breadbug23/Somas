8098d79c975d17943e7232187d1af5b6e5dca457
Piksqu <65417571+breadbug23@users.noreply.github.com>
2021-09-19 14:32:37 +0200

    Delete surgeroombamecha.js

--- scripts/units/surgeroombamecha.js
+++
@@ -1,5 +0,0 @@
-const surgeroombamecha = extendContent(UnitType, "surgeroombamecha", {});
-
-surgeroombamecha.defaultController = () => new MinerAI()
-
-surgeroombamecha.constructor = () => extend(UnitEntity, {});
