3c94e82d4bb5f67190c88a15e360cd084395aeac
Piksqu <65417571+breadbug23@users.noreply.github.com>
2021-09-19 14:31:11 +0200

    Delete graphiteroombamecha.js

--- scripts/units/graphiteroombamecha.js
+++
@@ -1,3 +0,0 @@
-const graphiteroombamecha = extend(UnitType, "graphiteroombamecha", {});
-graphiteroombamecha.defaultController = () => new MinerAI()
-graphiteroombamecha.constructor = () => extend(UnitWaterMove, {});
