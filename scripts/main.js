a1887e8406cc319d79e247a8f4166dd52226aea3
Piksqu <65417571+breadbug23@users.noreply.github.com>
2022-01-23 21:52:10 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,18 @@
 //units
-//support naval//
+//support naval
 require("units/shige")
 require("units/kashi")
 require("units/seka")
 require("units/meise")
 require("units/gemei")
-//specialist air//
+//specialist air
 require("units/zapper")
 require("units/taser")
 require("units/lightning")
 require("units/thunder")
 require("units/storm")
 //blocks
-require("block/phasereweaver")
-require("block/polycultivator")
-require("block/cryofluidcentrifuge")
+require("block/neodymiumwall")
 //reconstructor
 require("reconstructor")
 //planet
